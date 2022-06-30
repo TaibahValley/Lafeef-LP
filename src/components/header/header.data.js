e53d958bd4c8a3f7197236c1d8d4d8fba15c88ad
aisha-jabri <aaljabri@taibahvalley.com.sa>
2022-06-30 11:47:21 +0300

    first commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    path: 'banner',
+    label: 'Home',
+  },
+  {
+    path: 'services',
+    label: 'About Lafeef',
+  },
+  // {
+  //   path: 'testimonials',
+  //   label: 'Testimonials',
+  // },
+  {
+    path: 'funding',
+    label: 'Get Funding',
+  },
+  {
+    path: 'investing',
+    label: 'Start Investing',
+  },
+  {
+    path: 'how-it-works',
+    label: 'How it works ',
+  },
+  {
+    path: 'news',
+    label: 'News',
+  },
+];
